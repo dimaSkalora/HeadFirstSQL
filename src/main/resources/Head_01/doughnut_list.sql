bcf5b45b9ee46b61045883ff8a724522615de75a
dimaSkalora <timon2@ukr.net>
2017-08-15 09:58:58 +0300

    Head_01

--- src/main/resources/Head_01/doughnut_list.sql
+++ src/main/resources/Head_01/doughnut_list.sql
@@ -1,1 +1,6 @@
-SELECT * FROM gregs_list.doughnut_list;
+CREATE TABLE doughnut_list (
+  doughnut_name varchar(10) NOT NULL,
+  doughnut_type varchar(6) NOT NULL,
+  doughnut_cost decimal(3,2) NOT NULL DEFAULT '1.00',
+  PRIMARY KEY (`doughnut_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
--- src/main/resources/Head_01/my_contacts.sql
+++ src/main/resources/Head_01/my_contacts.sql
@@ -1,1 +1,12 @@
-SELECT * FROM headfirstsql.my_contacts;
+CREATE TABLE my_contacts (
+  list_name varchar(30) NOT NULL,
+  first_name varchar(20) DEFAULT NULL,
+  email varchar(50) DEFAULT NULL,
+  gender char(1) DEFAULT NULL,
+  birthday date DEFAULT NULL,
+  profession varchar(50) DEFAULT NULL,
+  location varchar(50) DEFAULT NULL,
+  status varchar(20) DEFAULT NULL,
+  interests varchar(100) DEFAULT NULL,
+  seeking varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
