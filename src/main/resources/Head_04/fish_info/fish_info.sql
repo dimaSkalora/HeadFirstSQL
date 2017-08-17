8f0687b709261585fba51d3cbaa03cf7dafc3a23
dimaSkalora <timon2@ukr.net>
2017-08-17 10:26:56 +0300

    Head_04

---
+++ src/main/resources/Head_04/fish_info/fish_info.sql
@@ -0,0 +1,6 @@
+CREATE TABLE fish_info (
+  common varchar(50) NOT NULL,
+  species varchar(50) NOT NULL,
+  location varchar(50) NOT NULL,
+  weight varchar(15) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
---
+++ src/main/resources/Head_04/fish_records/fish_records.sql
@@ -0,0 +1,9 @@
+CREATE TABLE fish_records (
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL,
+  common varchar(50) NOT NULL,
+  location varchar(50) NOT NULL,
+  state varchar(2) NOT NULL,
+  weight varchar(15) NOT NULL,
+  date date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8
