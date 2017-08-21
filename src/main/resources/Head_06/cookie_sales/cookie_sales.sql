fe91d65eda2632cc54eb7a60094eb7ed1b7dbda6
dimaSkalora <timon2@ukr.net>
2017-08-21 12:35:04 +0300

    Head_06

---
+++ src/main/resources/Head_06/cookie_sales/cookie_sales.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cookie_sales (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) NOT NULL,
+  `sales` decimal(4,2) NOT NULL,
+  `sale_date` date NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8
